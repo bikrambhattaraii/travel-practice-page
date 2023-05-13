733c1c030f7987d3e06a4acd30d2cea57403b291
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-05-13 20:14:46 +0545

    update

--- src/component/aboutus.jsx
+++ src/component/aboutus.jsx
@@ -1,14 +1,36 @@
-import "./aboutusstyles.css"
+import "./aboutusstyles.css";
 
-export function AboutUs(){
-
-    return(
-        <>
-        <div className="aboutus">
-            <h1>Our history</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum soluta, minus dicta pariatur ea eum nisi quia ipsam natus at, eligendi laborum qui eaque, nam distinctio laudantium rerum delectus ex.</p>
-            
-        </div>
-        </>
-    )
+export function AboutUs() {
+  return (
+    <>
+      <div className="aboutus">
+        <h1>About Us</h1>
+        <p>
+          Welcome to Dulantey, your go-to destination for travel inspiration and
+          information! At Dulantey, we believe that travel is one of the most
+          enriching experiences a person can have. It broadens horizons,
+          introduces new cultures, and helps us learn more about the world and
+          ourselves. That's why we're committed to providing you with the most
+          up-to-date and reliable travel advice and resources. Our team of
+          experienced writers and travel enthusiasts works tirelessly to bring
+          you the best travel tips, advice, and guides from all around the
+          world. Whether you're planning a weekend getaway, a family vacation,
+          or a solo adventure, we've got you covered. On our website, you'll
+          find a wealth of information on destinations both near and far,
+          including in-depth guides, reviews of top attractions, and insider
+          tips from locals. We cover everything from budget travel to luxury
+          vacations, so no matter what your travel style or budget, you'll find
+          something to inspire you. At Dulantey, we're passionate about
+          sustainable and responsible travel. We believe that travel should be a
+          force for good, and we work hard to promote environmentally friendly
+          and socially responsible travel practices. We're committed to helping
+          our readers make informed choices about their travel, so that they can
+          enjoy their adventures while also making a positive impact on the
+          world. Thank you for joining us on our journey at Dulantey. We hope to
+          inspire you to explore new horizons, make unforgettable memories, and
+          discover the beauty of the world around us.
+        </p>
+      </div>
+    </>
+  );
 }
